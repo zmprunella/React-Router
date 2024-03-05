14a52338d9ce97bde3a0db40df5c7790d38f2d89
ZMP <zane.prunella@gmail.com>
2024-03-04 17:30:03 -0800

    Navigating between pages with Link

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Products from "./pages/Products";
+import Home from "./pages/Home";
 
-const router = createBrowserRouter([{ path: "/", element: <Home /> }]);
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/products", element: <Products /> },
+]);
 
 function App() {
   return <RouterProvider router={router} />;
---
+++ src/pages/Products.js
@@ -0,0 +1,5 @@
+function Products() {
+  return <h1>The Products</h1>;
+}
+
+export default Products;
